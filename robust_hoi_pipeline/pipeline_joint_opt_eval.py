aed6025ff977d96396b97d4c628247c0546925d7
simba.wang <simba1986.wang@gmail.com>
2026-07-27 09:03:20 +0800

    [ENH] Update eval_fn_dict to use correct evaluation functions for mask_iou and depth_consistency

--- robust_hoi_pipeline/pipeline_joint_opt_eval.py
+++ robust_hoi_pipeline/pipeline_joint_opt_eval.py
@@ -29,9 +29,9 @@ eval_fn_dict = {
     "image_info": eval_m.eval_image_info,
     "mpjpe_ra_r": eval_m.eval_mpjpe_right,
     "cd_f_right": eval_m.eval_cd_f_right,
-    "mask_iou": eval_mask_iou,
-    "depth_consistency": eval_depth_consistency,
-    "penetration_volume": eval_penetration_volume,
+    "mask_iou": eval_m.eval_mask_iou,
+    "depth_consistency": eval_m.eval_depth_consistency,
+    # "penetration_volume": eval_m.eval_penetration_volume,
 }
 
 
